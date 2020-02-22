9a8bf57800f2324f11232ca45b33e0ad8e1444cb
Sandro Donner <account@donner-nsu.de>
2020-02-22 09:40:03 +0100

    Changed name of the application and added AssemblyInfo

--- TolarianCopyshop/ScreenPresenter/Properties/AssemblyInfo.cs
+++ TolarianCopyshop/ScreenPresenter/Properties/AssemblyInfo.cs
@@ -7,12 +7,12 @@ using System.Windows;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ScreenPresenter")]
+[assembly: AssemblyTitle("Tolarian Copyshop")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ScreenPresenter")]
-[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyProduct("Tolarian Copyshop")]
+[assembly: AssemblyCopyright("Copyright © Tolarian 2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -53,3 +53,4 @@ using System.Windows;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguage("en")]
