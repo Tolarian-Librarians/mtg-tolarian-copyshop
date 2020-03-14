11a23cf7df553201ff4c7da9ecc2fac68809ce8d
Sandro Donner <account@donner-nsu.de>
2020-03-14 09:27:09 +0100

    Applied the correct width of the listview. Now there is no horiontal scrollbar

---
+++ TolarianCopyshop/Frontend.WPF/Converter/IntSubstractionConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Tolarian.Copyshop.ScreenPresenter.Converter
+{
+    public class IntSubstractionConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is double numValue && double.TryParse(parameter as string, out double numPara))
+            {
+                return numValue - numPara;
+            }
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is double numValue && double.TryParse(parameter as string, out double numPara))
+            {
+                return numValue + numPara;
+            }
+            return value;
+        }
+    }
+}
