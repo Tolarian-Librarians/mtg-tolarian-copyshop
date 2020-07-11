1dddbb8cf426065741301ea6542a68cf8b6d97bd
janHildebrandt98 <>
2020-07-12 01:33:45 +0200

    Restored files

---
+++ TolarianCopyshop/Business/Models/Enums/CardType.cs
@@ -0,0 +1,16 @@
+﻿namespace Tolarian.Copyshop.Business.Models.Enums
+{
+    public enum CardType
+    {
+        Creature,
+        Sorcery,
+        Instant,
+        Land,
+        Planeswalker,
+        Enchantment,
+        Artifact,
+        Unknown,
+        Token,
+        Emblem
+    }
+}
---
+++ TolarianCopyshop/Controller/ResponseObjects/GetDeckStatisticsResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using Tolarian.Copyshop.Controller.ResponseObjects.Enums;
+
+namespace Tolarian.Copyshop.Controller.ResponseObjects
+{
+    public class GetDeckStatisticsResponse
+    {
+        public Dictionary<MtgColor, int> ColorSymbolCounts { get; set; }
+        public Dictionary<CardType, int> CardTypeCounts { get; set; }
+        public Dictionary<float, int> ManaCurve { get; set; }
+    }
+}
