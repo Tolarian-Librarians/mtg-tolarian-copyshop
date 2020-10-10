93eaad17c8597eba232b2336fc3bcc6b80b17a70
Sandro Donner <account@donner-nsu.de>
2020-10-11 01:23:28 +0200

    Implemented UI logic for export as text and import from url

---
+++ TolarianCopyshop/Frontend.WPF/Views/MultiLineInputChildView.xaml.cs
@@ -0,0 +1,31 @@
+﻿using MahApps.Metro.SimpleChildWindow;
+using Tolarian.Copyshop.ScreenPresenter.Base;
+using Tolarian.Copyshop.ScreenPresenter.ViewModels;
+
+namespace Tolarian.Copyshop.ScreenPresenter.Views
+{
+    /// <summary>
+    /// Interaktionslogik für MultiLineInput.xaml
+    /// </summary>
+    public partial class MultiLineInputChildView : ChildWindow
+    {
+        public MultiLineInputChildView() : this(string.Empty) { }
+
+        public MultiLineInputChildView(string startText)
+        {
+            this.InitializeComponent();
+            this.DataContext = new MultiLineInputViewModel(new Command(this.HandleAffirmativeCommand), new Command(this.HandleNegativeCommand));
+            (this.DataContext as MultiLineInputViewModel).MultiLineText = startText;
+        }
+
+        private void HandleAffirmativeCommand(object _)
+        {
+            if (this.DataContext is MultiLineInputViewModel viewModel)
+            {
+                this.Close(viewModel.MultiLineText);
+            }
+        }
+
+        private void HandleNegativeCommand(object _) => this.Close();
+    }
+}
