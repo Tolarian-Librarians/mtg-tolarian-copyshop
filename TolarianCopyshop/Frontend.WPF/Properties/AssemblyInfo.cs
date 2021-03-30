c21775b6a216ad77a2c8c49f5e8405d8353797e5
Sandro Donner <account@donner-nsu.de>
2021-03-30 10:36:45 +0200

    Updated AssemblyInfo

--- TolarianCopyshop/Frontend.WPF/Properties/AssemblyInfo.cs
+++ TolarianCopyshop/Frontend.WPF/Properties/AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Tolarian")]
 [assembly: AssemblyProduct("Tolarian Copyshop")]
-[assembly: AssemblyCopyright("Copyright © Tolarian 2020")]
+[assembly: AssemblyCopyright("Copyright © Tolarian 2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -47,5 +47,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 [assembly: NeutralResourcesLanguage("en-US")]
-[assembly: AssemblyVersion("1.4.1.0")]
-[assembly: AssemblyFileVersion("1.4.1.0")]
+[assembly: AssemblyVersion("1.4.2.0")]
+[assembly: AssemblyFileVersion("1.4.2.0")]
